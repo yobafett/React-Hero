41dd43c67400a7da6ce0039280b624d6e7df55f4
Yoba Fett <alexka.680@gmail.com>
2022-09-09 08:12:55 +0900

    Insert CharInfo component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,18 +1,21 @@
-import {Component} from 'react';
+import { Component } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
 class CharList extends Component {
+    constructor(props) {
+        super(props);
 
-    state = {
-        charList: [],
-        loading: true,
-        error: false
+        this.state = {
+            charList: [],
+            loading: true,
+            error: false
+        }
+
+        this.marvelService = new MarvelService();
     }
-    
-    marvelService = new MarvelService();
 
     componentDidMount() {
         this.marvelService.getAllCharacters()
@@ -37,18 +40,19 @@ class CharList extends Component {
     // Этот метод создан для оптимизации, 
     // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
-        const items =  arr.map((item) => {
-            let imgStyle = {'objectFit' : 'cover'};
+        const items = arr.map((item) => {
+            let imgStyle = { 'objectFit': 'cover' };
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-                imgStyle = {'objectFit' : 'unset'};
+                imgStyle = { 'objectFit': 'unset' };
             }
-            
+
             return (
-                <li 
+                <li
                     className="char__item"
-                    key={item.id}>
-                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-                        <div className="char__name">{item.name}</div>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
+                    <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+                    <div className="char__name">{item.name}</div>
                 </li>
             )
         });
@@ -62,12 +66,12 @@ class CharList extends Component {
 
     render() {
 
-        const {charList, loading, error} = this.state;
-        
+        const { charList, loading, error } = this.state;
+
         const items = this.renderItems(charList);
 
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
         const content = !(loading || error) ? items : null;
 
         return (
