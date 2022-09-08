660ca9becb2101ffe85365828091dfa64395c46c
Yoba Fett <alexka.680@gmail.com>
2022-09-08 19:26:33 +0900

    Update error message style

--- src/components/errorMessage/errorMessage.js
+++ src/components/errorMessage/errorMessage.js
@@ -3,9 +3,7 @@ import errorGif from './error.gif';
 const ErrorMessage = () => {
     const style = {
         display: 'block',
-        width: '250px',
         height: '250px',
-        objectFit: 'contain',
         margin: '0 auto',
     }
 
