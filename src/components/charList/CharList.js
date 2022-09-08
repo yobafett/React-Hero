ec44e199389764fc32380a1b0085dae2418c110d
Yoba Fett <alexka.680@gmail.com>
2022-09-08 19:25:35 +0900

    Insert char list item component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,46 +1,20 @@
 import './charList.scss';
 import abyss from '../../resources/img/abyss.jpg';
+import CharListItem from '../charListItem/charListItem';
+import nextId from "react-id-generator";
 
 const CharList = () => {
+    const charsCount = 9;
+    const chars = [];
+
+    for (let i = 0; i < charsCount; i++) {
+        chars.push(<CharListItem key={nextId()}/>)
+    }
+
     return (
         <div className="char__list">
             <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
+                {chars}
             </ul>
             <button className="button button__main button__long">
                 <div className="inner">load more</div>
