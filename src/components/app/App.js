170b41dfd7ea77e996d26385ac484a5cba7d3194
Yoba Fett <alexka.680@gmail.com>
2022-09-10 12:01:47 +0900

    Add ErrorBoundary

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,7 +4,10 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
+
 import { Component } from "react";
+
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 class App extends Component {
     state = {
@@ -25,7 +28,9 @@ class App extends Component {
                     <RandomChar />
                     <div className="char__content">
                         <CharList onCharSelected={this.onCharSelected} />
-                        <CharInfo charId={this.state.selectedChar} />
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar} />
+                        </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision" />
                 </main>
