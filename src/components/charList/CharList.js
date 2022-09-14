a633af63e4760e7b500de61d477d79bc6d25f1a9
Yoba Fett <alexka.680@gmail.com>
2022-09-14 21:06:31 +0900

    upd

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,9 +1,9 @@
 import { React, Component } from 'react';
 import PropTypes from 'prop-types';
 
-import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
 
 import './charList.scss';
 
