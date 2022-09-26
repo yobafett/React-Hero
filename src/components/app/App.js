16483ba5fbd1a0b21012b9c543d79ec997237c88
Yoba Fett <alexka.680@gmail.com>
2022-09-26 20:23:44 +0900

    Add routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,38 +1,25 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { MainPage, ComicsPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from '../comicsList/ComicsList';
-import AppBanner from '../appBanner/AppBanner';
-
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-    const [selectedChar, setSelectedChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setSelectedChar(id);
-    }
-
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                <AppBanner />
-                <ComicsList />
-                {/* <RandomChar />
-                <div className="char__content">
-                    <CharList onCharSelected={onCharSelected} />
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" /> */}
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage />
+                        </Route>
+                        <Route path="/comics">
+                            <ComicsPage />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 }
 
